1a1e708a6e8bc377471c5e9fdb7cbaa9f2f97a80
a137054853 <137054853@qq.com>
2023-04-27 03:16:37 +0800

    home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,85 @@
 <script setup>
-import hear from "./hear/Sound.vue"
-import vision from "./vision/index.vue"
+// import hear from "./hear/Sound.vue"
+// import vision from "./vision/index.vue"
+import {
+    Check,
+    Delete,
+    Edit,
+    Message,
+    Search,
+    Star,
+} from '@element-plus/icons-vue'
+
+const options = [
+    {
+        value: 'Option1',
+        label: 'Option1',
+    },
+    {
+        value: 'Option2',
+        label: 'Option2',
+    },
+    {
+        value: 'Option3',
+        label: 'Option3',
+    },
+    {
+        value: 'Option4',
+        label: 'Option4',
+    },
+    {
+        value: 'Option5',
+        label: 'Option5',
+    },
+]
 </script>
 
 <template>
-  <el-tabs type="border-card">
-    <el-tab-pane>
-      <template #label>
-        <span class="custom-tabs-label">
-          <span>言语识别采集</span>
-        </span>
-      </template>
-      <hear></hear>
-    </el-tab-pane>
-    <el-tab-pane>
-      <template #label>
-        <span class="custom-tabs-label">
-          <span>言语测试视觉辅助</span>
-        </span>
-      </template>
-      <vision></vision>
-    </el-tab-pane>
-  </el-tabs>
+    <div class="common-layout">
+        <el-container>
+            <el-header>Header</el-header>
+            <el-main>
+                <el-select v-model="value" filterable placeholder="Select">
+                    <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+                </el-select>
+            </el-main>
+            <el-footer>
+                <el-row class="mb-4">
+                    <el-button type="primary" round>Primary</el-button>
+                    <el-button type="success" round>Success</el-button>
+                </el-row>
+
+            </el-footer>
+        </el-container>
+    </div>
 </template>
 
 <style scoped>
-.custom-tabs-label {
-  margin-left: 0px;
-  margin-top: 0px;
-  width: 400px;
-  /* background-color: #819323; */
+html,
+body,#app {
+    margin-top: 0px;
+    margin-left: 0px;
 
 }
-.el-tabs__content {
-  padding: 32px;
-  color: #6b778c;
-  font-size: 32px;
-  
+
+.common-layout {
+    width: 1080px;
+    height: 540px;
+    margin-top: 0px;
 }
-.el-tab-pane{
-  height: 500px;
+
+.el-header {
+    height: 110px;
+    background-color: aqua;
+}
+
+.el-main {
+    height: 480px;
+    background-color: beige;
+}
+
+.el-footer {
+    height: 110px;
+    background-color: rgb(96, 96, 56);
 }
 </style>
