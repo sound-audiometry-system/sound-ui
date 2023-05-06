0f4d9ee8f3fe375afd4f194355e25bed7674a93a
18080448510 <1220411440@qq.com>
2023-05-06 20:12:17 +0800

    添加语言测试index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import index from '../views/home/HomeIndex.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: index
+      component: HomeView
     },
     {
       path: '/speaker',
@@ -17,6 +16,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/speaker/speakerTest.vue')
+    },
+    {
+      path:'/vision',
+      name:'vision',
+      component: () => import('../views/vision/index.vue')
     },
     
   ]
