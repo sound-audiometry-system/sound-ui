6ee8bda6f3831f0786dbebbfdc2ff8d96ff8e461
a137054853 <137054853@qq.com>
2023-05-07 10:37:20 +0800

    听力测试

--- src/views/speaker/hearTest.vue
+++ src/views/speaker/hearTest.vue
@@ -5,21 +5,45 @@
     <el-container>
         <el-aside>
             <div class="es-switch">
-                <span ><el-switch v-model="value1" :before-change="beforeChange1" /><label style="font-size: 16px;">左显示器</label></span>
-                <span ><el-switch v-model="value2" class="ml-2" /><label>右显示器</label> </span>
+                <span><el-switch v-model="value1" :before-change="beforeChange1" /><label
+                        style="font-size: 16px;">左显示器</label></span>
+                <span><el-switch v-model="value2" class="ml-2" /><label>右显示器</label> </span>
             </div>
             <div style="margin-left: 10px; width: 320px;height: 240px; background-color: #8cdcfe;"></div>
 
-            <div style="margin-left: 10px; width: 400px;height: 230px; background-color: #8cdcfe; margin-top: 35px;">
-                答题区域
-            </div>
+            <el-row class="error-a">
+                <label style="margin-left: 5px;font-size: large;">错误走向</label>
+                <el-button  size="small" style="margin-right: 5px;"><el-icon style="color: red;margin-right: 2px;"><CircleClose /></el-icon>错误</el-button>
+                <!-- <el-icon><CircleCloseFilled /></el-icon>   :icon="CircleClose"-->
+            </el-row>
+            <el-row style="margin-left: 10px; width: 400px;height: 200px; background-color: #8cdcfe;">
+                错误展示
+            </el-row>
         </el-aside>
-        
-        <el-main>Main</el-main>
+
+        <el-main>
+            <el-button style="margin: 3px 0px 5px 0px;" link><el-icon style="margin-right: 2px;" circle ><Microphone /></el-icon>开启录音</el-button>
+            <div style="height: 280px;width: 465px; background-color: blueviolet;">音响控件区域</div>
+            <el-row class="el-btn a">
+                <el-button size="small">开始</el-button>
+                <el-button size="small">保存</el-button>
+                <el-button size="small">保存并生成记录</el-button>
+                <el-button size="small">提前结束</el-button>
+                <el-button size="small">测试文件</el-button>
+            </el-row>
+            <el-row class="el-btn b">
+                <el-button size="small">上一个</el-button><el-button size="small">下一个</el-button><el-button
+                    size="small">重复</el-button>
+            </el-row>
+            <el-row>
+                <div style="height: 110px;width: 465px;background-color: aquamarine;">答题进度</div>
+            </el-row>
+        </el-main>
     </el-container>
 </template>
 <script setup lang="ts">
 import { ref } from "vue"
+import { Edit, CircleClose } from '@element-plus/icons-vue'
 let testName = ref("儿童视觉测试")
 let value1 = ref(true)
 let value2 = ref(true)
@@ -33,11 +57,40 @@ let beforeChange1 = (value1) => {
     return value1
 }
 
-
 </script>
 
 <style lang="scss" scoped>
-.el-form-item{
+.error-a {
+    margin-left: 10px;
+    width: 400px;
+    height: 45px;
+    background-color: #e0e0e0;
+    margin-top: 35px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.el-btn {
+    margin-top: 5px;
+    display: flex;
+    width: 465px;
+    align-items: center;
+
+}
+
+.a {
+    height: 48px;
+    background-color: #e9e9e9;
+    justify-content: space-around;
+}
+
+.b {
+    height: 60px;
+    justify-content: flex-start;
+}
+
+.el-form-item {
     margin-top: 3px;
     margin-bottom: 3px;
 }
@@ -47,27 +100,23 @@ let beforeChange1 = (value1) => {
 
     .el-aside {
         width: 500px;
-        background-color: rgb(173, 226, 226);
     }
-
     .el-main {
-        
-        background-color: bisque;
+     padding: 0px;
     }
 }
-.es-switch{
+
+.es-switch {
     display: flex;
     justify-content: space-between;
     width: 320px;
     margin-left: 10px;
-    .ml-2 {
-        
-    }
 }
+
 .el-aside {
     padding: 3px 5px;
     display: inline-block;
 
-    
+
 }
 </style>
--- src/views/vision/Index.vue
+++ src/views/vision/Index.vue
@@ -2,98 +2,100 @@
 <!--  -->
 <template>
   <el-container>
-        <el-header></el-header>
-        <el-main style="display: flex;justify-content: center;background-color: #f2f3f5;">
-          <div style="width: 50%;height: 100%;background-color: white;flex-direction: column;">
-            <div style="display: flex;margin:3% 3% 0 3%;flex-direction: column;">
-              <el-input  v-model="inputUserId"  placeholder="请输入" class="input-with-select" clearable @clear="userSearchData=[]" @blur="searchUser">
-                <template #prepend>
-                  <el-button :icon="Search" />
-                </template>
-              </el-input>
-              <div class="userItem" v-for="(item,idx) in userSearchData" :key="idx" v-if="userSearchData.length >0">
-                <div style="display: flex;align-items: center;margin:2% 2% 0 2%">
-                  <Female class="icon" v-if="item.sex==1"/>
-                  <Male class="icon" v-else/>
-                  <text style="margin-left: 4%;font-size: revert;font-weight: bold;">{{item.userId}}</text>
-                </div>
-                <el-button type="success" size="small" style="text-align: end;margin-right: 2%;" @click="toTest(item)">开始测试</el-button>
-              </div>
-              <el-empty description="暂无" v-else/>
-          </div>
-          </div>
-        </el-main>
-        <el-footer>
-          <div class="footer-div" :class="{ 'activeColor' :active ==1 }" @click="chooseItem(1)">
-            <Tickets class="icon"  color="white" />
-            <text  class="font">听力测试</text>
-          </div>
-          <div class="footer-div" :class="{ 'activeColor' :active ==2 }" @click="chooseItem(2)">
-            <Setting class="icon" color="white"/>
-            <text class="font" >配置检查</text>
-          </div>
-          <div class="footer-div" :class="{ 'activeColor' :active ==3 }" @click="chooseItem(3)">
-            <Monitor class="icon" color="white"/>
-            <text class="font" >言语实景模拟</text>
-          </div>
-        </el-footer>
-        <el-dialog   v-model="chooseTypeVisble" width="40%" center :close-on-click-modal="false">
-          <template #header="{ close, titleId, titleClass }">
-            <div style="display: flex;justify-content: left;">
-              <Setting class="icon" />
-              <text style="margin-left: 2%;font-weight: bold;">开始测试</text>
-            </div>
-          </template>
-          <div style="display: flex;flex-direction: column;">
-            <div style="display: flex;">
-              <text style="font-weight: bold;">用户ID:</text>
-              <div style="margin-left: 2%;display: flex;align-items: center;">
-                <Female width="1.2em" height="1.2em" v-if="userInfo.sex==1"/>
-                <Male width="1.2em" height="1.2em" v-else/>
-                <text style="margin-left: 1%;font-weight: bold;">{{ userInfo.userId }}</text>
-              </div> 
-            </div>
-            <text style="font-weight: bold;margin-top: 2%;">测试选择</text>
-            <el-select v-model="testResult" placeholder="请选择" style="margin-top: 2%;" no-data-text="暂无数据">
-              <el-option v-for="(item,idx) in testData" :key="idx" :label="item.name" :value="item.id"/>
-            </el-select>
-           
-          </div>
-          <template #footer>
-              <el-button color="#f2f3f5" @click="chooseTypeVisble = false">取消</el-button>
-              <el-button color="rgb(180,190,90)" @click="chooseTypeVisble = false">确定</el-button>
+    <el-header></el-header>
+    <el-main style="display: flex;justify-content: center;background-color: #f2f3f5;">
+      <div style="width: 50%;height: 100%;background-color: white;flex-direction: column;">
+        <div style="display: flex;margin:3% 3% 0 3%;flex-direction: column;">
+          <el-input v-model="inputUserId" placeholder="请输入" class="input-with-select" clearable @clear="userSearchData = []"
+            @blur="searchUser">
+            <template #prepend>
+              <el-button :icon="Search" />
             </template>
-        </el-dialog>
+          </el-input>
+          <div class="userItem" v-for="(item, idx) in userSearchData" :key="idx" v-if="userSearchData.length > 0">
+            <div style="display: flex;align-items: center;margin:2% 2% 0 2%">
+              <Female class="icon" v-if="item.sex == 1" />
+              <Male class="icon" v-else />
+              <text style="margin-left: 4%;font-size: revert;font-weight: bold;">{{ item.userId }}</text>
+            </div>
+            <el-button type="success" size="small" style="text-align: end;margin-right: 2%;"
+              @click="toTest(item)">开始测试</el-button>
+          </div>
+          <el-empty description="暂无" v-else />
+        </div>
+      </div>
+    </el-main>
+    <el-footer>
+      <div class="footer-div" :class="{ 'activeColor': active == 1 }" @click="chooseItem(1)">
+        <Tickets class="icon" color="white" />
+        <text class="font">听力测试</text>
+      </div>
+      <div class="footer-div" :class="{ 'activeColor': active == 2 }" @click="chooseItem(2)">
+        <Setting class="icon" color="white" />
+        <text class="font">配置检查</text>
+      </div>
+      <div class="footer-div" :class="{ 'activeColor': active == 3 }" @click="chooseItem(3)">
+        <Monitor class="icon" color="white" />
+        <text class="font">言语实景模拟</text>
+      </div>
+    </el-footer>
+    <el-dialog v-model="chooseTypeVisble" width="40%" center :close-on-click-modal="false">
+      <template #header="{ close, titleId, titleClass }">
+        <div style="display: flex;justify-content: left;">
+          <Setting class="icon" />
+          <text style="margin-left: 2%;font-weight: bold;">开始测试</text>
+        </div>
+      </template>
+      <div style="display: flex;flex-direction: column;">
+        <div style="display: flex;">
+          <text style="font-weight: bold;">用户ID:</text>
+          <div style="margin-left: 2%;display: flex;align-items: center;">
+            <Female width="1.2em" height="1.2em" v-if="userInfo.sex == 1" />
+            <Male width="1.2em" height="1.2em" v-else />
+            <text style="margin-left: 1%;font-weight: bold;">{{ userInfo.userId }}</text>
+          </div>
+        </div>
+        <text style="font-weight: bold;margin-top: 2%;">测试选择</text>
+        <el-select v-model="testResult" placeholder="请选择" style="margin-top: 2%;" no-data-text="暂无数据">
+          <el-option v-for="(item, idx) in testData" :key="idx" :label="item.name" :value="item.id" />
+        </el-select>
+
+      </div>
+      <template #footer>
+        <el-button color="#f2f3f5" @click="chooseTypeVisble = false">取消</el-button>
+        <el-button color="rgb(180,190,90)" @click="chooseTypeVisble = false">确定</el-button>
+      </template>
+    </el-dialog>
   </el-container>
 </template>
 <script setup lang="ts" >
 import { ref } from 'vue'
-import { Search} from '@element-plus/icons-vue'
+import { Search } from '@element-plus/icons-vue'
 
 const active = ref(1)
 
-const chooseTypeVisble =ref(false)
+const chooseTypeVisble = ref(false)
 //定义输入的用户id
-const inputUserId =ref("")
+const inputUserId = ref("")
 //存储选择的用户信息
-const userInfo=ref({
-  sex:null,
-  userId:''
+const userInfo = ref({
+  sex: null,
+  userId: ''
 })
 
 //搜索的用户结果,sex为1为男性，sex为2为女性
-const userSearchData = ref([{sex:1,userId:"000032"},{sex:2,userId:"000033"}])
+const userSearchData = ref([{ sex: 1, userId: "000032" }, { sex: 2, userId: "000033" }])
 //测试列表
-const testData =ref([{name:"听力测试",id:1},{name:"配置检查",id:2},{name:"言语实景模拟",id:3}])
+const testData = ref([{ name: "听力测试", id: 1 }, { name: "配置检查", id: 2 }, { name: "言语实景模拟", id: 3 }])
 //测试结果
-const testResult =ref()
+const testResult = ref()
 
 //远程搜索用户信息
-const searchUser =()=>{
+const searchUser = () => {
   //后续为远程搜索用户信息
 }
 
-const loadTestData =()=>{
+const loadTestData = () => {
   //后续为远程获取测试结果
 }
 
@@ -101,66 +103,73 @@ const loadTestData =()=>{
 loadTestData()
 
 //跳转到测试页面
-const toTest =(info)=>{
-  inputUserId.value =info.userId
-  userInfo.value =info
+const toTest = (info) => {
+  inputUserId.value = info.userId
+  userInfo.value = info
   chooseTypeVisble.value = true
 }
 
 //footer下面图标切换颜色
-const chooseItem = (val) =>{
+const chooseItem = (val) => {
   active.value = val
   //后续为头部切换页面
 }
 </script>
 <style lang="scss" scoped>
-  .font{
-    font-size:small;
-    font-weight: bold;
-    color: white;
+.font {
+  font-size: small;
+  font-weight: bold;
+  color: white;
+}
+
+.userItem {
+  display: flex;
+  margin-top: 3%;
+  background-color: #f2f3f5;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.icon {
+  width: 1.5em;
+  height: 1.5em;
+}
+
+.activeColor {
+  background-color: rgb(180, 190, 90);
+
+  .icon {
+    color: rgb(30, 25, 25)
   }
-  .userItem{
+
+  .font {
+    color: rgb(30, 25, 25)
+  }
+}
+
+.el-container {
+  width: 1080px;
+  height: 800px;
+
+  .el-header {
+    background-color: rgb(180, 190, 90);
+  }
+
+  .el-footer {
+    background-color: rgb(30, 25, 25);
     display: flex;
-    margin-top: 3%; 
-    background-color: #f2f3f5;
-    width: 100%;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
   }
-  .icon{
-    width: 1.5em; 
-    height: 1.5em; 
-  }
-  .activeColor{
-    background-color: rgb(180,190,90);
-    .icon{
-      color: rgb(30,25,25)
-    }
-    .font{
-      color: rgb(30,25,25)
-    }
-  }
-  .el-container {
-    width: 1080px;
-    height: 800px;
 
-   .el-header{
-    background-color: rgb(180,190,90);
-   }
-   .el-footer{
-     background-color: rgb(30,25,25);
-     display: flex;
-     justify-content: center;
-     align-items: center;
-   }
-   .footer-div{
-      width: 10%;
-      height: 100%;
-      margin-left: 5%;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-   }
-}
-</style>
+  .footer-div {
+    width: 10%;
+    height: 100%;
+    margin-left: 5%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+}</style>
