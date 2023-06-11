02e6518fa9a789ae82f57b45cdc6f9c9b482424f
mrhua <huangyong@live.cn>
2023-06-11 17:17:32 +0800

    111

--- src/components/sound/index.vue
+++ src/components/sound/index.vue
@@ -8,7 +8,7 @@
       <el-col class="dot" :span="12">
         <div class="dot-outer">
           <div class="dot-center"><img src="../../assets/header.png" width="30" alt=""></div>
-          <div @click="handleClkItem(index)" v-for="(item, index) in 12" :key="index" class="dot-control">
+          <div @click="handleClkItem(index)" v-for="(item, index) in soundList" :key="index" :class="{ 'dot-control': true, 'error-active': item.active && index <= 4, 'success-active': item.active && index > 5 && index <= 10 }">
             {{ index }}
           </div>
         </div>
@@ -22,15 +22,81 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { auditionApi } from "@/serve/api/user";
+import { findLastKey } from 'lodash';
 const emit = defineEmits(['handleClkItem'])
-const soundList = ref([])
-onMounted(async ()=> {
-  const res = await auditionApi.getDevice()
-  if (res.code == 0) {
-    console.log(res)
+const soundList = ref([
+  {
+    value: '0',
+    active: false,
+  },
+  {
+    value: '1',
+    active: false,
+  },
+  {
+    value: '2',
+    active: false,
+  },
+  {
+    value: '3',
+    active: false,
+  },
+  {
+    value: '4',
+    active: false,
+  },
+  {
+    value: '5',
+    active: false,
+  },
+  {
+    value: '6',
+    active: false,
+  },
+  {
+    value: '7',
+    active: false,
+  },
+  {
+    value: '8',
+    active: false,
+  },
+  {
+    value: '9',
+    active: false,
+  },
+  {
+    value: '10',
+    active: false,
+  },
+  {
+    value: '11',
+    active: false,
+  },
+
+])
+type Props = {
+  sounds: any;
+};
+const props = defineProps<Props>();
+watch(()=> props.sounds, (newValue, oldValue)=> {
+  if (props.sounds && props.sounds.length != 0) {
+    for (const item of props.sounds) {
+    for (const obj of soundList.value) {
+      if (obj.value == item.substring(1,2)) {
+        item.active = true
+      }
+    }
   }
+  }
+})
+onMounted(async ()=> {
+  // const res = await auditionApi.getDevice()
+  // if (res.code == 0) {
+  //   console.log(res)
+  // }
 })
 const handleClkItem = (index: number) => {
     emit('handleClkItem', index)
