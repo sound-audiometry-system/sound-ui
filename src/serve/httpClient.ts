7e3300c7fec227e0d6baf74be05d4f8ec5788c03
芶巍 <666.qq.com>
2023-07-30 16:02:20 +0800

    111

--- src/serve/httpClient.ts
+++ src/serve/httpClient.ts
@@ -67,9 +67,9 @@ class Api {
 // const baseUrl = 'http://192.168.10.18:8081'
 // const baseUrl = 'https://192.168.2.171:8080'
 // const baseUrl = 'https://localhost:8080'
-const baseUrl = 'http://localhost:8081'
-// const baseUrl = 'https://192.168.163.26:8080'
-// const baseUrl = 'http://192.168.10.18:8081'
+// const baseUrl = 'http://localhost:8081'
+// const baseUrl = 'https://192.168.10.54:8080'
+const baseUrl = 'http://192.168.0.102:8081'
 const api = new Api({
   baseURL: baseUrl,
   timeout: 10 * 1000,
